eb7ee4677abc0c386370dba81289d1a74c2c5604
Jordan Wu <jordan@n138-p24.eduroam.kth.se>
2019-09-03 17:59:25 +0200

    part 1 done

--- lab1/part1.py
+++ lab1/part1.py
@@ -1,35 +1,72 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def generateData():
+
+def generateData(features, mean, sigma, n):
     """
     Returns 2 sets of data A and B
     Each dataset has shape (#features, #samples)
     """
 
-    n = 100
-    mA, sigmaA = [1.0, 0.5], 0.5
-    mB, sigmaB = [-2.0, 0.0], 0.5
+    distA = sigma * np.random.randn(features, n)
 
-    distA = sigmaA * np.random.randn(2, n)
-    distB = sigmaB * np.random.randn(2, n)
+    distA[0] = distA[0] + mean[0]
+    distA[1] = distA[1] + mean[1]
 
-    distA[0] = distA[0] + mA[0]
-    distA[1] = distA[1] + mA[1]
+    return distA
 
-    distB[0] = distB[0] + mB[0]
-    distB[1] = distB[1] + mB[1]
 
-    return distA, distB
-
-def initializeWeights():
-    return np.random.randn(1, 2)
+def initializeWeights(input_dim, output_dim):
+    return np.random.randn(output_dim, input_dim)
 
 def forwardPass(W, x):
     return np.matmul(W, x)
 
-A, B = generateData()
-W = initializeWeights()
-print (forwardPass(W,A))
+def error(T, Y):
+    return np.sum((T - Y) ** 2) / 2
 
-plt.plot(A[0], A[1], "ro", B[0], B[1], "bo")
+if __name__ == "__main__":
+
+    eta = 0.001
+
+    # Training Data sets A and B
+    mA, sigmaA = [1.0, 0.5], 0.5
+    mB, sigmaB = [-2.0, 0.0], 0.5
+
+    A = generateData(2, mA, sigmaA, 100)
+    B = generateData(2, mB, sigmaB, 100)
+    X = np.hstack([A, B])
+    # Add biasing term
+    X = np.vstack([X, np.ones(X.shape[1])])
+
+    # Targets
+    T_A, T_B = np.ones(A.shape[1]), -1 * np.ones(A.shape[1])
+    T = np.hstack([T_A, T_B])
+
+    W = initializeWeights(X.shape[0], 1)
+    print (W.shape)
+
+
+    for _ in range(100):
+
+        Y = forwardPass(W, X)
+        print("error:", error(T, Y))
+        dW = -eta * np.matmul((Y - T), np.transpose(X))
+        W = W + dW
+
+
+    plt.plot(A[0], A[1], "ro", B[0], B[1], "bo")
+
+    def line(x, normal):
+        # 0 = w0x + w1y + w2
+        # y = (-w0x - w2) / w1
+        return (-normal[:,0] * x - normal[:,2]) / normal[:,1]
+
+    maxX = X[:, np.argmax(X[1])][0]
+    minX = X[:, np.argmin(X[1])][0]
+
+    x = np.arange(minX, maxX, 0.05)
+
+    plt.plot(x, line(x, W))
+
+    plt.show()
