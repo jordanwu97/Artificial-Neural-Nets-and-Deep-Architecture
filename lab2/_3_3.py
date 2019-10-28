a31139f58a6b0db2fb91ac0e9c60e109d2c30464
Jordan Wu <jordan@Jordans-MacBook-Pro.local>
2019-10-28 22:15:29 +0100

    3.3

--- lab2/_3_3.py
+++ lab2/_3_3.py
@@ -1,18 +1,5 @@
 from _3_1 import *
 
-from sklearn.metrics.pairwise import rbf_kernel
+from sklearn.metrics import pairwise_distances_argmin
 
-Y = np.arange(20)
-
-X = np.arange(10)
-
-var = 3
-
-n = RBF_NET(Y, 3, sign)
-
-phi = n.phi(X)
-
-
-phi2 = rbf_kernel(X.reshape(-1,1), Y.reshape(-1,1), gamma=1/(2*var**2))
-
-print (np.allclose(phi2,phi))
+# do stuff here, pairwise_distances_argmin is quite useful
